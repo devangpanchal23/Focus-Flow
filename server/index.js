c01f733e4f2aefdc0304559e01bb986a4f88d00e
Devang <Devang@parths-Mac-mini.local>
2026-05-03 16:34:03 +0530

    update index.js port number from 5001 to 5000 for perfect deployment of backend

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ import { handleRazorpayWebhook } from './controllers/webhookController.js';
 
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 // Trigger restart
 
 // Middleware
