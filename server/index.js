9f54cf2f7709ed5256886004f50f75dbfe8a2615
Devang <Devang@parths-Mac-mini.local>
2026-05-03 17:38:47 +0530

    feat: add root health check route

--- server/index.js
+++ server/index.js
@@ -122,7 +122,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(distPath));
 
-  app.get('/*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 } else {
